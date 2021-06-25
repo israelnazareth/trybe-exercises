ce1dc6b46950b41fd5dac90005537e10c0bdf2c9
Israel S. Nazareth <israel.fab@hotmail.com>
2021-06-25 15:52:27 -0300

    Adicionando baby steps comentados nos ex01 e 02

--- fundamentos/bloco_4/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/ex01.js
+++ fundamentos/bloco_4/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/ex01.js
@@ -1,5 +1,10 @@
 // Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log():
 
+// baby steps:
+// 1. Adicionar a array;
+// 2. 'percorra' significa criar uma estrutura de repetição 'for';
+// 3. 'imprimindo' significa mostrar os valores com o 'console.log'.
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let index = 0; index < numbers.length; index += 1){
