075e01bd10cccd9ce06f17fee58ad6d6b5450ced
Israel S. Nazareth <israel.fab@hotmail.com>
2021-06-25 17:29:38 -0300

    enviando a pasta bloco 4

---
+++ fundamentos/bloco_4/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/ex01.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 10;
+
+let sum = a + b
+console.log(sum)
+
+let sub = a - b
+console.log(sub)
+
+let mult = a * b
+console.log(mult)
+
+let div = a / b
+console.log(div)
+
+let mod = a % b
+console.log(mod)
