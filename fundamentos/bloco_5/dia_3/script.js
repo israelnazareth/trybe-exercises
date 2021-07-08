7511ac16ae272aa0a92e3113f0bda0baa1df82ba
Israel S. Nazareth <israel.fab@hotmail.com>
2021-07-07 23:01:49 -0300

    Adicionando for e if para criaçao das classes holiday e friday

--- fundamentos/bloco_5/dia_3/script.js
+++ fundamentos/bloco_5/dia_3/script.js
@@ -15,6 +15,7 @@ createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 
+// Adicionando os dias do mês no calendário;
 function daysOfTheMonth() {
   const dezDays = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   const daysList = document.querySelector('#days');
@@ -23,18 +24,19 @@ function daysOfTheMonth() {
     const daysMonth = value;
     const dezDaysItem = document.createElement('li');
     dezDaysItem.innerHTML = daysMonth;
-    dezDaysItem.setAttribute('class', 'day')
+    dezDaysItem.setAttribute('class', 'day');
     
     daysList.appendChild(dezDaysItem);
-  };  
-  document.getElementsByClassName('day')[25].classList.add('holiday')
-  document.getElementsByClassName('day')[26].classList.add('holiday')
-  document.getElementsByClassName('day')[32].classList.add('holiday')
-
-  document.getElementsByClassName('day')[5].classList.add('friday')
-  document.getElementsByClassName('day')[12].classList.add('friday')
-  document.getElementsByClassName('day')[19].classList.add('friday')
-  document.getElementsByClassName('day')[26].classList.add('friday')
+  };
+  // Adicionando as classes 'holiday' e 'friday' nos dias especificados;
+  for (let value in dezDays) {
+    if (value == 25 || value == 26 || value == 32) {
+      document.getElementsByClassName('day')[value].classList.add('holiday');
+    }
+    if (value == 5 || value == 12 || value == 19 || value == 26) {
+      document.getElementsByClassName('day')[value].classList.add('friday');
+    }
+  }
 };
 
 daysOfTheMonth();
