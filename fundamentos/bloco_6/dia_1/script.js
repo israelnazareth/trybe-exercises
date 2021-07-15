56f9911203866fa2cfdf5c3a7d6b1a430c5efdb4
Israel S. Nazareth <israel.fab@hotmail.com>
2021-07-14 22:50:36 -0300

    Envio de alguns requisitos no EX 02 e 03

--- fundamentos/bloco_6/dia_1/script.js
+++ fundamentos/bloco_6/dia_1/script.js
@@ -1,5 +1,6 @@
 let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espirito Santo', 'Goiás', 'Maranhão', 'Mato Grosso do Sul', 'Mato Grosso', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
 let selecionar = document.querySelector('#estado');
+const button = document.querySelector('#submit');
 
 function selectState() {
   for (let cadaEstado of estados) {
@@ -8,3 +9,17 @@ function selectState() {
     selecionar.appendChild(criarOpcao);
   }
 } selectState();
+
+// function inputDate() {
+//   let data = document.querySelector('#data');
+//   let day = '';
+//   let month = '';
+//   let year = '';
+//   if (day <= 0 || day > 31) {
+//     alert("Dia inválido");
+//   } else if (month <= 0 || month > 12) {
+//     alert("Mês inválido");
+//   } else if (year <= 0) {
+//     alert("Ano inválido")
+//   }
+// } inputDate();
