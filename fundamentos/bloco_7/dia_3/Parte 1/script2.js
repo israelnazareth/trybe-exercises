f6ce70dad7a1957480413afe6b1602c6cd80fe57
Israel S. Nazareth <israel.fab@hotmail.com>
2021-07-29 11:04:07 -0300

    Parte 1 - Exercicio 2

---
+++ fundamentos/bloco_7/dia_3/Parte 1/script2.js
@@ -0,0 +1,28 @@
+// 2. A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array:
+// - Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado;
+// - Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4];
+// - Verifique se o array passado por parâmetro não sofreu alterações;
+// - Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado;
+
+
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+};
+
+assert.strictEqual(typeof myRemove, 'function');
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+const randomArray = [10, 20, 30, 40, 50];
+myRemove(randomArray, 60);
+assert.deepStrictEqual(randomArray, [10, 20, 30, 40, 50]);
+
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
