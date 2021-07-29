4ec97bf35741a1babbf59b929e3b75df7410c0f2
Israel S. Nazareth <israel.fab@hotmail.com>
2021-07-29 10:14:15 -0300

    Parte 1 - Exercicio 1

--- fundamentos/bloco_7/dia_3/Parte 1/script1.js
+++ fundamentos/bloco_7/dia_3/Parte 1/script1.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { serialize } = require('v8');
 
 function sum(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
@@ -8,4 +9,8 @@ function sum(a, b) {
   return a + b;
 }
 
-// implemente seus testes aqui
+assert.strictEqual(typeof sum, 'function'); // Verifica se o tipo de 'sum' é uma 'function';
+assert.strictEqual(sum(4, 5), 9); // Verifica se a soma está correta;
+assert.strictEqual(sum(0, 0), 0); // Verifica se a soma de 0 + 0 é == 0;
+assert.throws(() => {sum(4, '5');}); // Lança um erro quando um parâmetro é !== de 'number';
+assert.throws(() => {sum(4, '5');}, /^Error: parameters must be numbers$/); // Especifica a mensagem de erro.
