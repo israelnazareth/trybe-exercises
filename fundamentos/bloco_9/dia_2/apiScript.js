a4c6439c141c753719ed10a6e28255cd1b87df62
Israel S. Nazareth <israel.fab@hotmail.com>
2021-08-12 22:42:37 -0300

    Exercicio 2

--- fundamentos/bloco_9/dia_2/apiScript.js
+++ fundamentos/bloco_9/dia_2/apiScript.js
@@ -6,10 +6,28 @@ const fetchJoke = () => {
     method: 'GET',
     headers: { 'Accept': 'application/json' }
   };
-
+  
   fetch(API_URL, myObject)
-    .then(response => response.json())
-    .then(data => document.getElementById('jokeContainer').innerText = data.joke);
+  .then(response => response.json())
+  .then(data => document.getElementById('jokeContainer').innerText = data.joke);
 };
 
 window.onload = () => fetchJoke();
+
+const fetchPromise = () => {
+  const promise = new Promise((resolve, reject) => {
+    const array = [];
+    for (let index = 1; index <= 10; index += 1) {
+      array.push(Math.floor(Math.random() * 50) + 1);
+    }
+    const sum = array.map((number) => number * number)
+    .reduce((sum, number) => sum + number);
+    console.log(sum);
+    (sum < 8000) ? resolve() : reject();
+  })
+  promise
+    .then(() => console.log('Promise resolvida'))
+    .catch(() => console.log('Promise rejeitada'));
+}
+
+fetchPromise();
